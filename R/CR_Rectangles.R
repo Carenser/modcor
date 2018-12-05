309eecc08413f1a1af548e2f931b35413092e11e
YassB@ck <yassine.benatmane@gmail.com>
2018-12-05 16:32:11 +0100

    eff colnames adaptation

--- R/CR_Rectangles.R
+++ R/CR_Rectangles.R
@@ -1,7 +1,7 @@
 #' Title
 #'
-#' @param cdc 
-#' @param eff 
+#' @param cdc
+#' @param eff
 #'
 #' @return
 #' @export
@@ -10,26 +10,26 @@
 #' @examples
 CR_RectangleSimple<-function(cdc,eff){
   for(i in 1:nrow(eff)){
-    if(i==1)successive<-0 else successive<-(eff$debut[i]<eff$fin[i-1]+eff$DMO[i-1]+30*60)#regle sur les activations successives d'une meme offre
-    heurefinPref<-as.POSIXct(floor(as.numeric(eff$debut[i]-eff$DMO[i]*60)/1800)*1800,origin="1970-01-01")#on arrondit au pas demi-horaire pr?s
-    if(successive==0)w<-which(cdc$HORODATE<heurefinPref & cdc$HORODATE>=heurefinPref-30*60)else print(paste("successive",paste(eff[i,],collapse="_")))
-    if(length(w)>0)cdc$PUISSANCE[cdc$HORODATE>eff$debut[i]-10*60 & cdc$HORODATE<eff$fin[i]]<-mean(cdc$PUISSANCE[w])else print(paste("Pas de reference pour ",paste(eff[i,],collapse="_")))
-    
-    cdc$SIGNE[cdc$HORODATE>eff$debut[i]-10*60 & cdc$HORODATE<eff$fin[i]] = eff$SIGNE[i]
+    if(i==1)successive<-0 else successive<-(eff$DEBUT[i]<eff$FIN[i-1]+eff$DMO[i-1]+30*60)#regle sur les activations successives d'une meme offre
+    heureFINPref<-as.POSIXct(floor(as.numeric(eff$DEBUT[i]-eff$DMO[i]*60)/1800)*1800,origin="1970-01-01")#on arrondit au pas demi-horaire pr?s
+    if(successive==0)w<-which(cdc$HORODATE<heureFINPref & cdc$HORODATE>=heureFINPref-30*60)else print(paste("successive",paste(eff[i,],collapse="_")))
+    if(length(w)>0)cdc$PUISSANCE[cdc$HORODATE>eff$DEBUT[i]-10*60 & cdc$HORODATE<eff$FIN[i]]<-mean(cdc$PUISSANCE[w])else print(paste("Pas de reference pour ",paste(eff[i,],collapse="_")))
+
+    cdc$SIGNE[cdc$HORODATE>eff$DEBUT[i]-10*60 & cdc$HORODATE<eff$FIN[i]] = eff$SIGNE[i]
   }
-  
+
   return(cdc)
 }
 
 CR_RectangleDouble<-function(cdc,eff){
   for(i in 1:nrow(eff)){
-    avant<-which(cdc$HORODATE<eff$debut[i] & cdc$HORODATE>=as.POSIXct(as.numeric(eff$debut[i])*2-as.numeric(eff$fin[i]),origin="1970-01-01"))
-    apres<-which(cdc$HORODATE>=eff$fin[i] & cdc$HORODATE<as.POSIXct(as.numeric(eff$fin[i])*2-as.numeric(eff$debut[i]),origin="1970-01-01"))
+    avant<-which(cdc$HORODATE<eff$DEBUT[i] & cdc$HORODATE>=as.POSIXct(as.numeric(eff$DEBUT[i])*2-as.numeric(eff$FIN[i]),origin="1970-01-01"))
+    apres<-which(cdc$HORODATE>=eff$FIN[i] & cdc$HORODATE<as.POSIXct(as.numeric(eff$FIN[i])*2-as.numeric(eff$DEBUT[i]),origin="1970-01-01"))
     if(length(avant)>0)Pavant<-mean(cdc$PUISSANCE[avant])else Pavant<-Inf
     if(length(apres)>0)Papres<-mean(cdc$PUISSANCE[apres])else Papres<-Inf
-    cdc$PUISSANCE[cdc$HORODATE>=eff$debut[i] & cdc$HORODATE<eff$fin[i]]<-ifelse(eff$SIGNE[i] < 0, max(Pavant,Papres), min(Pavant,Papres))
+    cdc$PUISSANCE[cdc$HORODATE>=eff$DEBUT[i] & cdc$HORODATE<eff$FIN[i]]<-ifelse(eff$SIGNE[i] < 0, max(Pavant,Papres), min(Pavant,Papres))
 
-    cdc$SIGNE[cdc$HORODATE>=eff$debut[i] & cdc$HORODATE<eff$fin[i]] = eff$SIGNE[i]
+    cdc$SIGNE[cdc$HORODATE>=eff$DEBUT[i] & cdc$HORODATE<eff$FIN[i]] = eff$SIGNE[i]
 
   }
   return(cdc)
@@ -42,7 +42,7 @@ CR_Histo<-function(cdc,eff,VARIANTE_HIST,DATE_INDISPO){#renvoie une courbe de r?
   Jrs<-Jrs[!Jrs %in% as.Date(DATE_INDISPO)]
   Jrs<-Jrs[order(Jrs)]
   for(i in 1:nrow(eff)){
-    J<-as.Date(substr(eff$debut[i],1,10))
+    J<-as.Date(substr(eff$DEBUT[i],1,10))
     Jrs2<-Jrs[Jrs<J]
     if(VARIANTE_HIST %in% c("MED4S","MOY4S")){
       Jrs2<-Jrs2[format(Jrs2,"%w")==format(J,"%w")]
@@ -50,13 +50,13 @@ CR_Histo<-function(cdc,eff,VARIANTE_HIST,DATE_INDISPO){#renvoie une courbe de r?
     }else{
       Jrs2<-Jrs2[max(1,length(Jrs2)-9):length(Jrs2)]#on utilise les 10 derniers jours
     }
-    w<-which(cdc$HORODATE>eff$debut[i]-10*60 & cdc$HORODATE<eff$fin[i])
+    w<-which(cdc$HORODATE>eff$DEBUT[i]-10*60 & cdc$HORODATE<eff$FIN[i])
     if(VARIANTE_HIST %in% c("MOY4S","MOY10J")){#en cas de non connaissance de la variante, on utilise MED10J
       for(w1 in w)cdc$PUISSANCE[w1]<-mean(cdc$PUISSANCE[cdc$jour %in% Jrs2 & cdc$heure %in% cdc$heure[w1]])
     }else{
       for(w1 in w)cdc$PUISSANCE[w1]<-median(cdc$PUISSANCE[cdc$jour %in% Jrs2 & cdc$heure %in% cdc$heure[w1]])
     }
-    
+
     cdc$SIGNE[w] = eff$SIGNE[i]
   }
   return(cdc[,names(cdc)[!names(cdc) %in% c("jour","heure")]])
@@ -70,9 +70,9 @@ CR_Prev <- function(cdcPrev,eff,cdc){
     cdcref<-numeric(0)
   }else{
     for(i in 1:nrow(eff)){
-      w<-which(cdcref$HORODATE > eff$debut[i]-10*60 & cdcref$HORODATE<eff$fin[i])
+      w<-which(cdcref$HORODATE > eff$DEBUT[i]-10*60 & cdcref$HORODATE<eff$FIN[i])
       cdcref$PUISSANCE[w]<-cdcref$prev[w]
-      
+
       cdcref$SIGNE[w] = eff$SIGNE[i]
     }
   }
